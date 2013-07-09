e219eee2cf1dac841520891599d69c3ba1b086da
Manuel Ayaso <auron1984@gmail.com>
2013-07-10 01:07:09 +0200

    Added reading form index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+
+var readFromIndex = function() {
+  var buf = fs.readFileSync('index.html');
+  return buf.toString();
+}
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var text = readFromIndex();
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
